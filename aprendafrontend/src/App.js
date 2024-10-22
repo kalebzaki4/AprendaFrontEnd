088c3da13f5781e73eb47e3cebd6d2c62e7d513a
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-10-22 17:20:08 -0300

    Projetto Inicial

---
+++ aprendafrontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bem-vindo ao AprendaFrontEnd!</h1>
+      <p>Vamos aprender React juntos! 🚀</p>
+    </div>
+  );
+}
+
+export default App;
